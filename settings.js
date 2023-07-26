43ee0dd9328b429f8cfbc38a0d9c495500af3e81
RianZenzei <132425448+Riansyah009@users.noreply.github.com>
2023-07-26 14:35:59 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const fs = require('fs')
 
-global.creator = 'RYNX// yourname
+global.creator = 'RokumoDev'// yourname
 global.MONGO_DB_URI = "mongodb+srv://riansyah5790:fxrg0zlasRbXmPyU@cluster0.udd6fu6.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
-global.ACTIVATION_TOKEN_SECRET = "SantayWir" //isi apa aja bebas
-global.your_email = "kanjutnegro@ryanerwinstore.icu" //email
+global.ACTIVATION_TOKEN_SECRET = "DexterKey" //isi apa aja bebas
+global.your_email = "ryoyamada.hub@gmail.com" //email
 global.email_password = "wdippplmorabohhl" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
